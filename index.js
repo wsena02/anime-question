341befb4136b1dbcbe63fecc16bcf36a64ef5ada
sena <115485799+wsena02@users.noreply.github.com>
2022-10-13 02:31:34 +0900

    アニメクイズy

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('baz').addEventListener("click",function(){
+    alert('②影山');
+    alert('③日向');
+});
+
+document.getElementById('hoge').addEventListener("click",function(){
+    alert('①北　信介');
+});
+
